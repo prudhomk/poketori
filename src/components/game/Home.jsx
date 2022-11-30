3eea060d6720b2500f0bc746933c01ccd29a3dff
Kalan Prudhomme <cleaver9@gmail.com>
2022-11-30 10:04:16 -0800

    jp version working as well, need to add translation for hint feature

--- src/components/game/Home.jsx
+++ src/components/game/Home.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from 'react-router';
 import { useTranslation } from 'react-i18next';
 import Header from './Header';
 import '../../i18n/config.js';
-import Pop from './Popover';
 import styles from '../styles/Main.scss';
 
 export default function Home() {
@@ -22,8 +21,6 @@ export default function Home() {
       <div className={styles.splash}>
         <h1>{t('title')}</h1>
         <button onClick={handleClick} data-cy="play">{t('home-button')}</button>
-
-        <Pop/>
       </div>
 
       {/* <div className={styles.version}>
